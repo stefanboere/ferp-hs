d03efa5ea1dbf0e17e6d027f178b4badd6630474
Stefan Boere <s.boere@hotmail.com>
2021-02-20 15:14:47 +0100

    Add some core icons

--- frontend/src/Components/Icon.hs
+++ frontend/src/Components/Icon.hs
@@ -12,14 +12,21 @@ module Components.Icon
   , icon
   , ferpIcon
   -- * Core icons
-  , successStandardIcon
-  , errorStandardIcon
-  , warningStandardIcon
-  , infoStandardIcon
-  , statusStandardIcon
-  , checkCircleIcon
-  , exclamationCircleIcon
   , angleIcon
+  , barsIcon
+  , checkCircleIcon
+  , cogIcon
+  , ellipsisHorizontalIcon
+  , ellipsisVerticalIcon
+  , errorStandardIcon
+  , exclamationCircleIcon
+  , infoStandardIcon
+  , searchIcon
+  , statusStandardIcon
+  , successStandardIcon
+  , timesIcon
+  , userIcon
+  , warningStandardIcon
   )
 where
 
@@ -182,3 +189,46 @@ ferpIcon = svg $ do
   path'
     ("style" =: "fill:none;stroke:#8fbcbb;stroke-width:1.5")
     "m 18.708098,27.224358 c -4.32253,0.0073 -8.585215,1.010805 -12.457031,2.932618 -0.9567525,0.475144 -1.5617363,1.45129 -1.5615233,2.519531 v 2.069824 H 32.826262 l -0.0015,-2.069824 c -6.3e-5,-1.068568 -0.605666,-2.044798 -1.562988,-2.519531 -3.901068,-1.936334 -8.198523,-2.940243 -12.553711,-2.932618 z"
+
+barsIcon :: (PostBuild t m, DomBuilder t m) => m ()
+barsIcon = svg $ do
+  path "M32,29H4a1,1,0,0,1,0-2H32a1,1,0,0,1,0,2Z"
+  path "M32,19H4a1,1,0,0,1,0-2H32a1,1,0,0,1,0,2Z"
+  path "M32,9H4A1,1,0,0,1,4,7H32a1,1,0,0,1,0,2Z"
+
+cogIcon :: (PostBuild t m, DomBuilder t m) => m ()
+cogIcon = svg $ do
+  path
+    "M18.1,11c-3.9,0-7,3.1-7,7s3.1,7,7,7c3.9,0,7-3.1,7-7S22,11,18.1,11z M18.1,23c-2.8,0-5-2.2-5-5s2.2-5,5-5c2.8,0,5,2.2,5,5S20.9,23,18.1,23z"
+  path
+    "M32.8,14.7L30,13.8l-0.6-1.5l1.4-2.6c0.3-0.6,0.2-1.4-0.3-1.9l-2.4-2.4c-0.5-0.5-1.3-0.6-1.9-0.3l-2.6,1.4l-1.5-0.6l-0.9-2.8C21,2.5,20.4,2,19.7,2h-3.4c-0.7,0-1.3,0.5-1.4,1.2L14,6c-0.6,0.1-1.1,0.3-1.6,0.6L9.8,5.2C9.2,4.9,8.4,5,7.9,5.5L5.5,7.9C5,8.4,4.9,9.2,5.2,9.8l1.3,2.5c-0.2,0.5-0.4,1.1-0.6,1.6l-2.8,0.9C2.5,15,2,15.6,2,16.3v3.4c0,0.7,0.5,1.3,1.2,1.5L6,22.1l0.6,1.5l-1.4,2.6c-0.3,0.6-0.2,1.4,0.3,1.9l2.4,2.4c0.5,0.5,1.3,0.6,1.9,0.3l2.6-1.4l1.5,0.6l0.9,2.9c0.2,0.6,0.8,1.1,1.5,1.1h3.4c0.7,0,1.3-0.5,1.5-1.1l0.9-2.9l1.5-0.6l2.6,1.4c0.6,0.3,1.4,0.2,1.9-0.3l2.4-2.4c0.5-0.5,0.6-1.3,0.3-1.9l-1.4-2.6l0.6-1.5l2.9-0.9c0.6-0.2,1.1-0.8,1.1-1.5v-3.4C34,15.6,33.5,14.9,32.8,14.7z M32,19.4l-3.6,1.1L28.3,21c-0.3,0.7-0.6,1.4-0.9,2.1l-0.3,0.5l1.8,3.3l-2,2l-3.3-1.8l-0.5,0.3c-0.7,0.4-1.4,0.7-2.1,0.9l-0.5,0.1L19.4,32h-2.8l-1.1-3.6L15,28.3c-0.7-0.3-1.4-0.6-2.1-0.9l-0.5-0.3l-3.3,1.8l-2-2l1.8-3.3l-0.3-0.5c-0.4-0.7-0.7-1.4-0.9-2.1l-0.1-0.5L4,19.4v-2.8l3.4-1l0.2-0.5c0.2-0.8,0.5-1.5,0.9-2.2l0.3-0.5L7.1,9.1l2-2l3.2,1.8l0.5-0.3c0.7-0.4,1.4-0.7,2.2-0.9l0.5-0.2L16.6,4h2.8l1.1,3.5L21,7.7c0.7,0.2,1.4,0.5,2.1,0.9l0.5,0.3l3.3-1.8l2,2l-1.8,3.3l0.3,0.5c0.4,0.7,0.7,1.4,0.9,2.1l0.1,0.5l3.6,1.1V19.4z"
+
+ellipsisVerticalIcon :: (PostBuild t m, DomBuilder t m) => m ()
+ellipsisVerticalIcon = svg $ do
+  circle (18, 4.9)  2.9
+  circle (18, 18)   2.9
+  circle (18, 31.1) 2.9
+
+ellipsisHorizontalIcon :: (PostBuild t m, DomBuilder t m) => m ()
+ellipsisHorizontalIcon = svg $ do
+  circle (4.9 , 18) 2.9
+  circle (18  , 18) 2.9
+  circle (31.1, 18) 2.9
+
+searchIcon :: (PostBuild t m, DomBuilder t m) => m ()
+searchIcon = svg $ do
+  path
+    "M16.33,5.05A10.95,10.95,0,1,1,5.39,16,11,11,0,0,1,16.33,5.05m0-2.05a13,13,0,1,0,13,13,13,13,0,0,0-13-13Z"
+  path "M35,33.29l-7.37-7.42-1.42,1.41,7.37,7.42A1,1,0,1,0,35,33.29Z"
+
+timesIcon :: (PostBuild t m, DomBuilder t m) => m ()
+timesIcon = svg $ do
+  path
+    "M19.41,18l8.29-8.29a1,1,0,0,0-1.41-1.41L18,16.59,9.71,8.29A1,1,0,0,0,8.29,9.71L16.59,18,8.29,26.29a1,1,0,1,0,1.41,1.41L18,19.41l8.29,8.29a1,1,0,0,0,1.41-1.41Z"
+
+userIcon :: (PostBuild t m, DomBuilder t m) => m ()
+userIcon = svg $ do
+  path
+    "M18,17a7,7,0,1,0-7-7A7,7,0,0,0,18,17ZM18,5a5,5,0,1,1-5,5A5,5,0,0,1,18,5Z"
+  path
+    "M30.47,24.37a17.16,17.16,0,0,0-24.93,0A2,2,0,0,0,5,25.74V31a2,2,0,0,0,2,2H29a2,2,0,0,0,2-2V25.74A2,2,0,0,0,30.47,24.37ZM29,31H7V25.73a15.17,15.17,0,0,1,22,0h0Z"
