b4cc3d2da41746a34f943b0138f6f53b8c167546
Stefan Boere <s.boere@hotmail.com>
2021-02-27 09:49:20 +0100

    Install reflex-dom-pandoc

--- default.nix
+++ default.nix
@@ -1,5 +1,7 @@
 { system ? builtins.currentSystem }:
-let unstable = import ./nix { };
+let
+  unstable = import ./nix { };
+  sources = import ./nix/sources.nix { };
 in (import ./reflex-platform { inherit system; }).project ({ pkgs, ... }: {
   useWarp = true;
   withHoogle = false;
@@ -24,13 +26,11 @@ in (import ./reflex-platform { inherit system; }).project ({ pkgs, ... }: {
       # Prevents ghcjs build being stuck, see reflex-platform#717
       mmorph = self.callHackage "mmorph" "1.1.3" { };
       clay = self.callHackage "clay" "0.13.3" { };
-      reflex-dom-contrib = doJailbreak (self.callCabal2nix "reflex-dom-contrib"
-        (pkgs.fetchFromGitHub {
-          owner = "reflex-frp";
-          repo = "reflex-dom-contrib";
-          rev = "11db20865fd275362be9ea099ef88ded425789e7";
-          sha256 = "1rmcqg97hr87blp1vl15rnvsxp836c2dh89lwpyb7lvh86d7jwaf";
-        }) { });
+      reflex-dom-contrib = doJailbreak
+        (self.callCabal2nix "reflex-dom-contrib" sources.reflex-dom-contrib
+          { });
+      reflex-dom-pandoc =
+        self.callCabal2nix "reflex-dom-pandoc" sources.reflex-dom-pandoc { };
     };
 
 })
